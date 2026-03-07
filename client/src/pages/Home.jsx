be5a90af75bed79bac9a55ded6cac028262f4c61
its-me-rbk <rubhak222@gmail.com>
2026-03-07 11:39:03 +0530

    Configure CORS and Vite proxy for frontend-backend communication

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import ConnectionTest from '../components/common/ConnectionTest';
 
 const Home = () => {
   return (
@@ -16,6 +17,11 @@ const Home = () => {
             Login
           </Link>
         </div>
+      </div>
+
+      {/* Add the connection test */}
+      <div style={{ maxWidth: '800px', margin: '0 auto', padding: '2rem' }}>
+        <ConnectionTest />
       </div>
 
       {/* Features Section (Optional) */}
